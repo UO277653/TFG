e4e26ab9a7eb14bce9cde3509152b885d56494cf
Stelian Adrian Stanci <UO277653@uniovi.es>
2023-04-12 21:35:22 +0200

    Primer commit, inicio de la app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import subprocess
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/execute', methods=['POST'])
+def execute_script():
+    output = subprocess.check_output(['python', 'maxcut.py'])
+    return output
+
+if __name__ == '__main__':
+    app.run()
