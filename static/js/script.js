aef7f986e4baef77e7490485c5633897befdc9e9
Stelian Adrian Stanci <UO277653@uniovi.es>
2023-05-13 19:03:56 +0200

    Añadido botón para seleccionar resolvedor

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,38 @@ var resultDiv = document.getElementById("cajaResultado");
 var executeFormMaxCutLocal = document.getElementById("execute-MaxCutLocal");
 var executeFormMaxCutReal = document.getElementById("execute-MaxCutReal");
 
+$(document).ready(function() {
+    $("#comoResolver").click(function() {
+        resultDiv.innerHTML = "";
 
+        var solver = document.getElementById("solver").value;
+        switch (solver) {
+            case "simuladorLocal":
+                resultDiv.innerHTML = "1";
+                break;
+            case "simuladorRemoto":
+                resultDiv.innerHTML = "2";
+                break;
+            case "ordenadorReal":
+                resultDiv.innerHTML = "3";
+                break;
+            case "annealer":
+                resultDiv.innerHTML = "4";
+                break;
+            case "annealerSimulatedAnnealingSolver":
+                resultDiv.innerHTML = "5";
+                break;
+            case "annealerTabuSolver":
+                resultDiv.innerHTML = "6";
+                break;
+            case "annealerSteepestDescentSolver":
+                resultDiv.innerHTML = "7";
+                break;
+        }
+    });
+});
+
+/**
 executeFormMaxCutLocal.addEventListener("submit", function(event) {
     event.preventDefault();
     resultDiv.innerHTML = "";
@@ -38,3 +69,5 @@ executeFormMaxCutReal.addEventListener("submit", function(event) {
     xhr.send(new FormData(executeFormMaxCutReal));
 
 });
+
+ **/
