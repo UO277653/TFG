7b800ef3c474d30aa4ed5ad428b612ec011e6844
Stelian Adrian Stanci <UO277653@uniovi.es>
2023-05-20 16:26:39 +0200

    Inicio de implantación del paso de parámetros a través de la aplicación

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,9 @@ var resultDiv = document.getElementById("cajaResultado");
 var executeFormMaxCutLocal = document.getElementById("execute-MaxCutLocal");
 var executeFormMaxCutReal = document.getElementById("execute-MaxCutReal");
 
+var arrayNodos = [];
+
+
 $(document).ready(function() {
     $("#comoResolver").click(function() {
         resultDiv.innerHTML = "";
@@ -42,7 +45,7 @@ function ejecutarScriptPython(scriptUrl) {
     $.ajax({
         type: "POST",
         url: scriptUrl,
-        //data: {script_name: scriptName},
+        data: JSON.stringify(arrayNodos),
         success: function(response) {
             console.log(response);
             resultDiv.appendChild(document.createTextNode(response));
@@ -55,41 +58,14 @@ function ejecutarScriptPython(scriptUrl) {
 }
 
 
-/**
-executeFormMaxCutLocal.addEventListener("submit", function(event) {
-    event.preventDefault();
-    resultDiv.innerHTML = "";
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', executeFormMaxCutLocal.action);
-    xhr.onload = function() {
 
-        resultDiv.appendChild(document.createTextNode(xhr.responseText));
+function agregarNodoMaxCut() {
+    var texto = document.getElementById("nodoMaxCut").value;
+    arrayNodos.push(texto);
+    console.log(arrayNodos);
+    limpiarTexto();
+}
 
-        const imgGrafico = document.createElement("img");
-        imgGrafico.src = "../static/img/graph.png";
-        resultDiv.appendChild(imgGrafico);
-
-    };
-    xhr.send(new FormData(executeFormMaxCutLocal));
-
-});
-
-executeFormMaxCutReal.addEventListener("submit", function(event) {
-    event.preventDefault();
-    resultDiv.innerHTML = "";
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', executeFormMaxCutReal.action);
-    xhr.onload = function() {
-
-        resultDiv.appendChild(document.createTextNode(xhr.responseText));
-
-        const imgGrafico = document.createElement("img");
-        imgGrafico.src = "../static/img/graph.png";
-        resultDiv.appendChild(imgGrafico);
-
-    };
-    xhr.send(new FormData(executeFormMaxCutReal));
-
-});
-
- **/
+function limpiarTexto() {
+    document.getElementById("nodoMaxCut").value = "";
+}
